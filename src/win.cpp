6c88043782b5bf04e8e500c03314e928e1b584e1
Thomas Lin Pedersen <thomasp85@gmail.com>
2019-04-05 14:45:52 +0200

    try to shim setenv/unsetenv for windows

---
+++ src/win.cpp
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+
+// Declared in init.cpp
+
+#ifdef _WIN32
+
+int setenv(const char *envname, const char *envval, int overwrite) {
+  return (int) _putenv_s(envname, envval);
+}
+
+int unsetenv(const char *name) {
+  char buf[1 + snprintf(NULL, 0, "%s=", name)];
+  snprintf(buf, sizeof(buf), "%s=", name);
+  return (int) _putenv(buf);
+}
+
+#endif
