a5c0e03e89b1c203b6f4520a00037b0933e2d420
Thomas Lin Pedersen <thomasp85@gmail.com>
2019-04-05 15:37:46 +0200

    less messy unsetenv shim

--- src/win.cpp
+++ src/win.cpp
@@ -1,5 +1,4 @@
 #include <stdlib.h>
-#include <cstdio>
 
 // Declared in init.cpp
 
@@ -10,9 +9,7 @@ int setenv(const char *envname, const char *envval, int overwrite) {
 }
 
 int unsetenv(const char *name) {
-  char buf[1 + snprintf(NULL, 0, "%s=", name)];
-  snprintf(buf, sizeof(buf), "%s=", name);
-  return (int) _putenv(buf);
+  return (int) _putenv_s(name, "");
 }
 
 #endif
