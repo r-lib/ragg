2f816082e89e483437f0f7e6f467a006198bed83
Elliott Sales de Andrade <quantum.analyst@gmail.com>
2020-10-01 04:11:41 -0400

    Fix capture on big endian systems (#49)
    * Byte swap `agg_capture` output on big-endian systems.
    
    R expects colours to be integers of ABGR, which on little-endian
    machines maps to singular bytes in RGBA order, but maps to ABGR order on
    bit-endian machines.
    
    * Use WORDS_BIGENDIAN from Rconfig.h.
    
    This seems to actually work, as __BIG_ENDIAN__ isn't defined on s390x.
    Possibly it needs another header, but it's easier to use a macro already
    in R.

--- src/ragg.h
+++ src/ragg.h
@@ -34,10 +34,12 @@ typedef agg::pixfmt_rgba32_pre                  pixfmt_type_32;
 typedef agg::pixfmt_rgb48_pre                   pixfmt_type_48;
 typedef agg::pixfmt_rgba64_pre                  pixfmt_type_64;
 
-#ifdef __BIG_ENDIAN__ 
+#ifdef WORDS_BIGENDIAN
 typedef agg::pixfmt_abgr32_plain                pixfmt_r_raster;
+typedef agg::pixfmt_abgr32_pre                  pixfmt_r_capture;
 #else
 typedef agg::pixfmt_rgba32_plain                pixfmt_r_raster;
+typedef agg::pixfmt_rgba32_pre                  pixfmt_r_capture;
 #endif
 
 // pixfmt agnosting demultiplying
