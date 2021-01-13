776588446a69a3d507b7e9243b2cafdb0a1c7d75
Jeroen Ooms <jeroenooms@gmail.com>
2021-01-13 22:20:03 +0100

    Windows: update system libs

--- tools/winlibs.R
+++ tools/winlibs.R
@@ -1,16 +1,7 @@
 VERSION <- commandArgs(TRUE)
-if(!file.exists(sprintf("../windows/cairo-%s/include/cairo/cairo.h", VERSION))){
+if(!file.exists(sprintf("../windows/harfbuzz-%s/include/png.h", VERSION))){
   if(getRversion() < "3.3.0") setInternet2()
-  download.file(sprintf("https://github.com/rwinlib/cairo/archive/v%s.zip", VERSION), "lib.zip", quiet = TRUE)
-  dir.create("../windows", showWarnings = FALSE)
-  unzip("lib.zip", exdir = "../windows")
-  unlink("lib.zip")
-}
-
-BASELIB <- "3.5.0"
-if(!file.exists(sprintf("../windows/baselibs-%s/include/png.h", BASELIB))){
-  if(getRversion() < "3.3.0") setInternet2()
-  download.file(sprintf("https://github.com/rwinlib/baselibs/archive/v%s.zip", BASELIB), "lib.zip", quiet = TRUE)
+  download.file(sprintf("https://github.com/rwinlib/harfbuzz/archive/v%s.zip", VERSION), "lib.zip", quiet = TRUE)
   dir.create("../windows", showWarnings = FALSE)
   unzip("lib.zip", exdir = "../windows")
   unlink("lib.zip")
